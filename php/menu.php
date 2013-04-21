45372e714fd88b53d5b118a9c2cbcc87f158bd61
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-21 14:44:46 +0200

    Updated main pages

--- php/menu.php
+++ php/menu.php
@@ -2,9 +2,9 @@
  <div id="sideBar">
     <p><a class="sideBar" href="index.html"> Home </a></p>
     <p><a class="sideBar" target="_blank" href="http://sourceforge.net/apps/gallery/aplpy/"> Gallery </a></p>
-    <p><a class="sideBar" target="_blank" href="https://github.com/aplpy/aplpy/downloads"> Download </a></p>
+    <p><a class="sideBar" target="_blank" href="https://pypi.python.org/pypi/APLpy"> Download </a></p>
     <p><a class="sideBar" href="install.html"> Install </a></p>
-    <p><a class="sideBar" href="documentation/index.html"> Documentation </a></p>
+    <p><a class="sideBar" href="http://aplpy.readthedocs.org"> Documentation </a></p>
     <p><a class="sideBar" href="http://groups.google.com/group/aplpy-users"> Discussion </a></p>
     <p><a class="sideBar" href="https://github.com/aplpy/aplpy/issues"> Report a bug </a></p>
     <p><a class="sideBar" target="_blank" href="https://github.com/aplpy/aplpy">GitHub</a></p>
